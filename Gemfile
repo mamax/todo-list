022b1df14786a4dcfe81147337b078f0ffc031e0
Maksym Mazurkevych <mmmazurkevych@gmail.com>
2015-08-12 07:38:28 +0300

    add secure passwords and tests

--- Gemfile
+++ Gemfile
@@ -8,6 +8,7 @@ gem 'pg', '0.18.2'
 gem 'sass-rails', '~> 5.0'
 gem 'bootstrap-sass', '2.3.2.0'
 gem 'bootstrap-will_paginate', '0.0.9'
+gem 'bcrypt-ruby', '3.1.2'
 # Use Uglifier as compressor for JavaScript assets
 gem 'uglifier', '>= 1.3.0'
 # Use CoffeeScript for .coffee assets and views
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -3,12 +3,15 @@ require 'spec_helper'
 
 describe User do
 
-  before { @user = User.new( email: "user@example.com", password: "password") }
+  before { @user = User.new( email: "user@example.com", password: "password", password_confirmation: "password") }
 
   subject { @user }
 
-  it { should respond_to(:password) }
   it { should respond_to(:email) }
+  it { should respond_to(:password_digest) }
+  it { should respond_to(:password) }
+  it { should respond_to(:password_confirmation) }
+  it { should respond_to(:authenticate) }
 
   it { should be_valid }
 
@@ -46,5 +49,38 @@ describe User do
     end
 
     it { should_not be_valid }
+  end
+
+  describe "when password is not present" do
+    before do
+      @user = User.new( email: "user@example.com", password: " ", password_confirmation: " ")
+    end
+    it { should_not be_valid }
+  end
+
+  describe "when password doesn't match confirmation" do
+    before { @user.password_confirmation = "mismatch" }
+    it { should_not be_valid }
+  end
+
+  describe "with a password that's too short" do
+    before { @user.password = @user.password_confirmation = "a" * 5 }
+    it { should be_invalid }
+  end
+
+  describe "return value of authenticate method" do
+    before { @user.save }
+    let(:found_user) { User.find_by(email: @user.email) }
+
+    describe "with valid password" do
+      it { should eq found_user.authenticate(@user.password) }
+    end
+
+    describe "with invalid password" do
+      let(:user_with_invalid_password) { found_user.authenticate("invalid") }
+
+      it { should_not eq user_with_invalid_password }
+      specify { expect(user_with_invalid_password).to be_falsey }
+    end
   end
 end
